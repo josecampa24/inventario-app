c56064b383fbde50c20b93db1aaa2fa62871bc37
josecampa24 <armandopro224@gmail.com>
2026-08-13 13:21:27 -0600

    chore: update Electron entry point to .cjs and add --no-sandbox flag to dev script

--- electron/main.cjs
+++ electron/main.cjs
@@ -51,7 +51,7 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, "preload.cjs"),
       contextIsolation: true,
       enableRemoteModule: false,
       nodeIntegration: false,
